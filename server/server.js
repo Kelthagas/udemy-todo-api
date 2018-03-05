a0bf9a6a4a333d24c1e2ef319f0f7820adb0338c
Kelthagas <batallonesa@gmail.com>
2018-03-05 07:55:29 -0800

    create single todo GET route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -27,6 +28,23 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+// Getting a single User data
+app.get('/todos/:id', (req, res) => {
+  req.params
+
+  if (!ObjectID.isValid(req.params.id)) {
+    res.status(404).send();
+  } else {
+    Todo.findById(req.params.id).then((todo) => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+
+      res.send({todo});
+    }, (e) => res.status(400).send());
+  }
 });
 
 app.listen(3000, () => {
